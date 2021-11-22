5dc8bb4d509be39436ce38714243caca07569338
arturoverbel <arturo-verbel@hotmail.com>
2021-11-22 15:58:12 -0500

    Setting port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 const moment = require("moment")
 
 const app = express()
-const PORT = 3000 
+const PORT = process.env.PORT || 5000 
 let visitas = 0;
 try {
     let data = fs.readFileSync("visitas");
